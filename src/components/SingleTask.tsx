6f0b5c402e84903143377168d91015525bff70e0
Pasitik <paasitik@gmail.com>
2023-05-31 17:16:41 +0000

    update

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,4 +1,5 @@
 import { Task } from '../model'
+import { useState, useRef, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
 
@@ -11,30 +12,60 @@ interface Props{
 
 const SingleTask = ({task, tasks, setTasks}: Props) => {
 
+    const [edit, setEdit] =useState<boolean>(false);
+    const [editTask, setEditTask] = useState<string>(task.task);
+
     const handleDone = (id: number) =>{
         setTasks(
             tasks.map((task)=>
                 task.id === id?{...task, isDone:!task.isDone} : task
             ))
     };
+
+    const handleDelete = (id: number) =>{
+        setTasks(tasks.filter((task)=>task.id !== id));
+    }
+
+    const handleEdit = (e:React.FormEvent, id:number) =>{
+        e.preventDefault();
+
+        setTasks(tasks.map((task)=>task.id === id?{...task, task:editTask} : task));
+        setEdit(false);
+    }
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(()=>{
+        inputRef.current?.focus();
+    },[edit])
     
   return (
-    <form className='flex lg:w-40 md:w-40 w-90 rounded-md p-6 mt-3 bg-yellow-400'>
+    <form onSubmit={(e)=>handleEdit(e, task.id)} className='flex lg:w-40 md:w-40 w-90 rounded-md p-6 mt-3 bg-yellow-400'>
         {
-            task.isDone ? (
-                <s className=" flex-1 p-1 border-none text-lg focus:outline-none">
-                    {task.task}
-                </s>
-            ):(
-                <span className=" flex-1 p-1 border-none text-lg focus:outline-none">
-                    {task.task}
-                </span>
+            edit ? (
+                <input type="text" ref={inputRef} value={editTask} onChange={(e)=>setEditTask(e.target.value)}/>
+            ):
+            (
+                task.isDone ? (
+                    <s className=" flex-1 p-1 border-none text-lg focus:outline-none">
+                        {task.task}
+                    </s>
+                ):
+                (
+                    <span className=" flex-1 p-1 border-none text-lg focus:outline-none">
+                        {task.task}
+                    </span>
+                )
             )
         }
         
         <div> 
-            <span className=" ml-5 text-lg cursor-pointer"> <FontAwesomeIcon icon={faPenToSquare} /> </span>
-            <span className="ml-5 text-lg cursor-pointer"> <FontAwesomeIcon icon={faTrash} /> </span>
+            <span className=" ml-5 text-lg cursor-pointer" onClick= {() => {
+                if (!edit && !task.isDone){
+                    setEdit(!edit);
+                }
+            }}> <FontAwesomeIcon icon={faPenToSquare} /> </span>
+            <span className="ml-5 text-lg cursor-pointer" onClick={() => handleDelete(task.id)}> <FontAwesomeIcon icon={faTrash} /> </span>
             <span className="ml-5 text-lg cursor-pointer" onClick={()=>handleDone(task.id)}> <FontAwesomeIcon icon={faCheck} /> </span>
 
         </div>
